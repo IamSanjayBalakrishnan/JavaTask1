d635c42d768637bdf075a2e1551609f89320c9d1
Administrator <iamsanjaybala@gmail.com>
2026-05-26 19:29:17 +0200

    initial commit

---
+++ src/main/java/pl/edu/vistula/sanjay/firstprojectjavaspring/FirstProjectJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.sanjay.firstprojectjavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectJavaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstProjectJavaSpringApplication.class, args);
+    }
+
+}
